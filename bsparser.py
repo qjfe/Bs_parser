5f9f0377f36982300e528f18b54d5a663dc11087
qjfe <rybalkin.k@gmail.com>
2014-04-05 13:46:36 +0400

    import mechanize

--- bsparser.py
+++ bsparser.py
@@ -1,4 +1,8 @@
-from BeautifulSoup import BeautifulSoup
-from BeautifulSoup import BeautifulStoneSoup
-import BeautifulSoup
+import mechanize
+from bs4 import BeautifulSoup
 
+br = mechanize.Browser()
+
+resp = br.open("http://ebay.com")
+html = resp.read()
+print html
